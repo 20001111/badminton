a9f222a8b8276d416da1ed5bb95d4dd4e4c3b71b
20001111 <58553749+20001111@users.noreply.github.com>
2025-07-30 01:44:12 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import React from 'react';
+import BadmintonBookingSystem from './BadmintonBookingSystem';
+
 function App() {
-  return <h1>Hello from React!</h1>;
+  return <BadmintonBookingSystem />;
 }
 
 export default App;
